5904e6e9783d1d026ea31d3c0bc0e333a3654344
root <root@emc-demo.emc-demo.i4.internal.cloudapp.net>
2015-04-22 11:47:14 +0000

    First Commit

--- emc/metadata.rb
+++ emc/metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures emc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends 'httpd', '~> 0.2'
+depends 'yum','~>0.0'
+depends 'mysql', '~> 6.0'
--- emc/recipes/default.rb
+++ emc/recipes/default.rb
@@ -33,3 +33,17 @@ service "jenkins" do
 	action [:start, :enable]
 end
 
+mysql_service 'default' do
+  version '5.7'
+  bind_address '0.0.0.0'
+  port '3306'  
+  data_dir '/data'
+  initial_root_password 'Ch4ng3me'
+  action [:create, :start]
+end
+
+execute 'link' do
+  command "ln -s /var/run/mysql-default/mysqld.sock /var/lib/mysql/mysql.sock"
+  action :run
+end
+
